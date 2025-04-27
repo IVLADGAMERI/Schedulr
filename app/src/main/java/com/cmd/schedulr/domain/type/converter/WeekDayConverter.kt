42125ed404733900ec122111a688f3db6f6de05d
Cmd_ <uchzhezherov@gmail.com>
2025-04-27 12:16:48 +0300

    Nullable support

--- app/src/main/java/com/cmd/schedulr/domain/type/converter/WeekDayConverter.kt
+++ app/src/main/java/com/cmd/schedulr/domain/type/converter/WeekDayConverter.kt
@@ -5,12 +5,12 @@ import com.example.schedulr.domain.type.WeekDay
 
 class WeekDayConverter {
     @TypeConverter
-    fun fromWeekDayToString(day: WeekDay): String {
-        return day.name
+    fun fromWeekDayToString(day: WeekDay?): String? {
+        return day?.name
     }
 
     @TypeConverter
-    fun toWeekDayFromString(data: String): WeekDay {
-        return WeekDay.valueOf(data)
+    fun toWeekDayFromString(data: String?): WeekDay? {
+        return data?.let { WeekDay.valueOf(data) }
     }
 }
